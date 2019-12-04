a216be9debfed9e7eb70d44ba3852ce82d2ce002
99818fhwn <99818@fhwn.ac.at>
2019-12-04 06:09:11 +0100

    Removed the V2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express Test App ♥ V2' });
+  res.render('index', { title: 'Express Test App ♥' });
 });
 
 module.exports = router;
