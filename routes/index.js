18f137ef68ca6dfdcd793b780103e00af90d509c
99818fhwn <99818@fhwn.ac.at>
2019-12-04 05:51:30 +0100

    Changed something

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express Test App ♥' });
+  res.render('index', { title: 'Express Test App ♥ V2' });
 });
 
 module.exports = router;
